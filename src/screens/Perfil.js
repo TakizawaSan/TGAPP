54f5f4ab925e0d58d61246c36242410d4ba5aac3
TakizawaSan <diegorobertogs@hotmail.com>
2020-09-28 19:41:46 -0300

    first commit

---
+++ src/screens/Perfil.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, ScrollView,StyleSheet } from 'react-native';
+import { Button, Card, Title, Paragraph,Divider,Avatar } from 'react-native-paper'; 
+import { Input } from 'react-native-elements';
+import PerfilComp from '../componentes/perfilComp'
+
+const pessoal = {
+  name:'Marcelo Rocha',
+    tel:'(16) 98171-7322',
+    end:'Avenida Carlos Fernandes',
+    num:'1377',
+    comp:'Centro'
+}
+const veiculo = [{
+  desc:'Astra Preto',
+  placa:'ADS124',
+  ano:'2000'},
+  {
+  desc:'Astra Preto',
+  placa:'ADS124',
+  ano:'2000'}
+]
+
+const Perfil = () => {
+  return (
+          <View>
+            <PerfilComp 
+              objP={pessoal} 
+              objV={veiculo}/>
+          </View>
+  )
+}
+
+export default Perfil;
